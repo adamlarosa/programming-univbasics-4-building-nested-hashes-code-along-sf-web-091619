5e05e4d0a04c617c56287e0f017c6e4284b512ec
Adam La Rosa <adam.larosa@gmail.com>
2019-09-05 21:29:37 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -27,4 +27,5 @@ def bonus
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
-puts bonus[:montague][:hero][:status]
+bonus[:montague][:hero][:status] = "dead"
+puts epic_tragedy[:montague][:hero][:status]
